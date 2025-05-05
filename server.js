cb09195dd36dcc4d14c429fc1f6cdfb5025d13b5
Daniella <daniellaabibi@gmail.com>
2025-05-05 01:50:10 +0100

    file control

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json());
 app.use(cors())
 
-const PORT= process.env.PORT || 1000;
+const PORT= process.env.PORT || 0;
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`)})
 
 //mongoose connect
